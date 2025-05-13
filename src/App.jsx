3b02bdc29f7a21cac29fb5dd62679dde7610ad25
MATI EZEQUIEL GRZENDA <48716278@ort>
2025-05-13 09:47:17 -0300

    Empezar parte 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 //import { useState } from 'react'
+import { useState } from 'react'
 import Titulo from './Titulo.jsx'
 import Formulario from './Formulario.jsx'
 import Listado from './Listado.jsx'
 import './App.css'
 
 function App() {
+  const [listaCitas, setLista] = useState();
+
   return (
     <>
-      <div id="root">
-        <Titulo titulo="Administración de Pacientes" />
-        <div className="container">
-          <div className="row">
-            <Formulario />
-            <Listado />
-          </div>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"></link>
+      <link href="https://fonts.googleapis.com/css?family=Staatliches" rel="stylesheet"></link>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css"></link>
+      <Titulo titulo="Administración de Pacientes" />
+      <div className="container">
+        <div className="row">
+          <Formulario listaCitas={listaCitas} setLista={setLista} />
+          <Listado listaCitas={listaCitas} setLista={setLista} />
         </div>
       </div>
     </>
